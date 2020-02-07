9478b015c36235d2a88769e7781f8e357a9e39b1
Adam Shelton <aqs@uchicago.edu>
2020-02-07 11:38:58 -0600

    Final cleaning and merging of crime data

--- Data/data_import_cleaning.R
+++ Data/data_import_cleaning.R
@@ -178,7 +178,14 @@ for (asset in latest_release$assets) {
   download.file(asset$browser_download_url, here("Data", asset$name))
 }
 
-
+extract_case_nums = function(x) {
+  x = x %>% str_split(" ") %>% sapply((function(x) x[length(x)]))
+  are_right_length = str_length(x) == 8
+  x[!are_right_length] = NA
+  are_cases = suppressWarnings(!is.na(str_sub(x, 3) %>% as.numeric()))
+  x[!are_cases] = NA
+  x
+}
 
 incident_data = list.files(here("Data"), pattern="ucpd_incident_data_scraped_*") %>% {here("Data", .)} %>% read_csv() %>% 
   fix_nas(c(":", "VOID", "Void", "void", "n/a", "N/A", "na", "NA", "No reports this date", "None")) %>% 
@@ -202,6 +209,9 @@ incident_data = list.files(here("Data"), pattern="ucpd_incident_data_scraped_*")
          warrant = str_detect(Incident, "warrant"),
          armed = str_detect(Incident, "armed"),
          aggravated = str_detect(Incident, "aggravated") | str_detect(Incident, "agg\\."),
+         domestic = str_detect(Incident, "domestic"),
+         cpd_case_id = extract_case_nums(Comments),
+         arrest = Disposition == "Arrest",
          coords = get_coords(Location),
          Incident = str_remove_all(Incident, "information") %>% 
            str_remove_all("assist other agency") %>% 
@@ -250,11 +260,93 @@ names(incident_data) = str_to_lower(names(incident_data))
 write_csv(incident_data, here("Data", "cleaned_incident_data.csv"))
 saveRDS(incident_data, here("Data", "cleaned_incident_data.rds"))
 
-# CPD Data
+### CPD Data ###
 
 # (41.845899, -87.608858)  (41.732987, -87.604720) (41.731920, -87.677545)
 #approximate 4 mile radius from 57th and Ellis Ave
-cpd_crimes_data = RSocrata::read.socrata("https://data.cityofchicago.org/resource/ijzp-q8t2.json?$where=year > 2009 AND latitude between '41.732987' and '41.845899' AND longitude > -87.677545", socrata_app_token) %>% select(-c(x_coordinate, y_coordinate, location.latitude, location.longitude, location.human_address)) %>% mutate(date = as_datetime(date), updated_on = as_datetime(updated_on), arrest = as.logical(arrest), domestic = as.logical(domestic), beat = factor(beat), district = factor(district), ward = factor(ward), community_area = factor(community_area), fbi_code = factor(fbi_code), year = as.numeric(year), latitude = as.numeric(latitude), longitude = as.numeric(longitude))
+cpd_crime_data = RSocrata::read.socrata("https://data.cityofchicago.org/resource/ijzp-q8t2.json?$where=year > 2009 AND latitude between '41.732987' and '41.845899' AND longitude > -87.677545", socrata_app_token) %>% 
+  select(-c(x_coordinate, y_coordinate, location.latitude, location.longitude, location.human_address)) %>%
+  mutate(date = as_datetime(date), 
+         updated_on = as_datetime(updated_on), 
+         arrest = as.logical(arrest), 
+         domestic = as.logical(domestic), 
+         beat = factor(beat), 
+         district = factor(district), 
+         ward = factor(ward), 
+         community_area = factor(community_area), 
+         fbi_code = factor(fbi_code), 
+         year = as.numeric(year), 
+         latitude = as.numeric(latitude), 
+         longitude = as.numeric(longitude),
+         aggravated = str_detect(str_to_lower(description), "agg") & !str_detect(str_to_lower(description), "non-agg"),
+         attempted = str_detect(str_to_lower(description), "att ") | str_detect(str_to_lower(description), "att:") | str_detect(str_to_lower(description), "attempt"), 
+         armed = str_detect(str_to_lower(description), "armed"))
 
-write_csv(cpd_crimes_data, here("Data", "cleaned_crime_data.csv"))
-saveRDS(cpd_crimes_data, here("Data", "cleaned_crime_data.rds"))
+write_csv(cpd_crime_data, here("Data", "cleaned_crime_data.csv"))
+saveRDS(cpd_crime_data, here("Data", "cleaned_crime_data.rds"))
+
+### MERGING DATA ###
+
+"%not_in%" = function(x, table) match(x, table, nomatch = 0) == 0
+
+new_categories = read_csv(here("Data", "variable_conversions.csv")) %>% mutate_all(str_squish)
+
+recategorize = function(x, old_cats, new_cats) {
+  if (length(old_cats) != length(new_cats)) {
+    stop("Lengths of category vectors must match!")
+  }
+  for (n in 1:length(old_cats)) {
+    x[x == old_cats[n]] = new_cats[n]
+  }
+  x
+}
+
+new_ucpd_cats = new_categories %>% filter(!is.na(ucpd_cat))
+ucpd_prep_crime_data = incident_data %>% 
+  mutate(responding_dept = ifelse(str_detect(disposition, "CPD"), "both", "ucpd"), reporting_dept = "ucpd") %>% 
+  select(`ucpdi#`, cpd_case_id, responding_dept, reporting_dept, start, incident, comments, building, arrest, domestic, armed, aggravated, attempted, lat, lon) %>% 
+  rename("id" = `ucpdi#`, "case_number" = cpd_case_id, "date" = start, "primary_type" = incident, "description" = comments, "location_description" = building) %>% 
+  filter(primary_type %not_in% c("traffic violation", "traffic accident", "injured person", "medical call", "fire", "mental health transport", "death investigation", "sick person", "medical call illness", "medical transport", "chemical spill", "dog bite", "elevator entrapment", "wellbeing check", "medical call sick person", "property", "missing person found", "well being check", "stuck elevator", "wellbeing", "traffc violation", "smoke report", "smoke", "s", "	person down", "on", "open door", "notification of order of protection", "non", "medical call ill person", "lost recovered property", "found person", "event", "animal bite", "ambulance request", "alarm", "al report", "accident report", "haz mat event", "hazardous material event", "hazmat incident", "miscellaneous incident report", "offense", "other property", "person down", "recovered motor vehicle foreign steal", "recovered stolen motor vehicle", "recovery automobile", "safety hazard offense", "security alarm", "stolen vehicle recovery", "activated alarm", "haz mat incident", "lost found property", "lostfound property", "found property", "motor vehicle recovery", "report", "recovered motor vehicle", "recovered vehicle", "report", "incident", "resisting", "complaint", "miscellaneous incident", "other", "hazardous material incident", "miscellaneous")) %>% 
+  mutate(primary_type = primary_type %>% str_squish() %>% recategorize(new_ucpd_cats$ucpd_cat, new_ucpd_cats$new_cat))
+
+new_cpd_cats = new_categories %>% filter(!is.na(cpd_cat))
+cpd_prep_crime_data = cpd_crime_data %>% 
+  mutate(responding_dept = ifelse(case_number %in% (ucpd_prep_crime_data %>% 
+                                                      select(case_number, responding_dept) %>% 
+                                                      filter(responding_dept == "both") %>% 
+                                                      .$case_number), 
+                                  "both", "cpd"), 
+         reporting_dept = "cpd") %>% 
+  select(id, case_number, responding_dept, reporting_dept, date, primary_type, description, location_description, arrest, domestic, armed, aggravated, attempted, latitude, longitude) %>% 
+  rename("lat" = latitude, "lon" = longitude) %>% 
+  mutate(primary_type = str_to_lower(primary_type), description = str_to_lower(description), location_description = str_to_lower(location_description), primary_type = primary_type %>% 
+           str_squish() %>% 
+           recategorize(new_cpd_cats$cpd_cat, new_cpd_cats$new_cat)) %>% 
+  filter(primary_type %not_in% c("non-criminal", "non - criminal", "non-criminal (subject specified)"))
+
+crime_cats = ucpd_prep_crime_data %>% count(primary_type, sort = T) %>% rename("ucpd_count" = n) %>% full_join(cpd_prep_crime_data %>% count(primary_type, sort = T) %>% rename("cpd_count" = n))
+
+common_vars = crime_cats %>% filter(!is.na(ucpd_count), !is.na(cpd_count)) %>% .$primary_type
+
+collapse_var = function(x, cats_to_keep, collapsed_name = "other") {
+  keep_orig = x %in% cats_to_keep
+  x[!keep_orig] = collapsed_name
+  x
+}
+
+merged_crime_data = bind_rows(ucpd_prep_crime_data, cpd_prep_crime_data) %>% mutate(primary_type = collapse_var(primary_type, common_vars))
+
+## Merging with census data
+
+fix_nans = function(x) {
+  x[is.nan(x)] = 0
+  x
+}
+
+census_data = readRDS(here("Data", "dec_blocks_2010_prop.rds"))
+merged_sf = merged_crime_data %>% filter(!is.na(lat), !is.na(lon)) %>% st_as_sf(coords = c("lon", "lat")) 
+st_crs(merged_sf) = st_crs(census_data)
+merged_final = merged_sf %>% st_join(census_data, join = st_within) %>% select(-c("NAME")) %>% rename("block_id" = GEOID) %>% mutate_if(is.character, factor) %>% st_drop_geometry() %>% mutate_if(is.numeric, fix_nans) %>% na.omit()
+
+write_csv(merged_final, here("Data", "merged_crime_data_final.csv"))
+saveRDS(merged_final, here("Data", "merged_crime_data_final.rds"))
